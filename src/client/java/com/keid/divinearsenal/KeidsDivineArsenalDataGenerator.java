361593bfea7924860f9fa66e3890d4d62261096c
very secret <lavaempteybukkit@gmail.com>
2024-12-27 01:08:51 +0100

    Initial commit

---
+++ src/client/java/com/keid/divinearsenal/KeidsDivineArsenalDataGenerator.java
@@ -0,0 +1,11 @@
+package com.keid.divinearsenal;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class KeidsDivineArsenalDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
